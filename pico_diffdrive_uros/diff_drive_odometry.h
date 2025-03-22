855a8046f8cdb312ca2fffb8809458202aaef37c
treedel <devesh.k1203@gmail.com>
2025-03-22 11:35:25 +0530

    Modified everything into a modular system. Added IMU Support

--- pico_diffdrive_uros/diff_drive_odometry.h
+++ pico_diffdrive_uros/diff_drive_odometry.h
@@ -4,17 +4,12 @@
     #include <math.h>
 
     typedef struct DiffDriveOdom {
-        float loop_period;
+        float odom_update_period;
         float wheel_radius;
         float wheel_separation;
 
-        float left_wheel_pos;
-        float right_wheel_pos;
-        float old_left_wheel_pos;
-        float old_right_wheel_pos;
-
-        float left_wheel_vel;
-        float right_wheel_vel;
+        float left_wheel_vel_mps;
+        float right_wheel_vel_mps;
 
         float linear_x;
         float angular_z;
@@ -26,11 +21,6 @@
     } DiffDriveOdom;
 
     void reset_odometry(DiffDriveOdom* odom) {
-        odom->left_wheel_pos = 0.0;
-        odom->right_wheel_pos = 0.0;
-        odom->old_left_wheel_pos = 0.0;
-        odom->old_right_wheel_pos = 0.0;
-
         odom->linear_x = 0.0;
         odom->angular_z = 0.0;
 
@@ -39,53 +29,47 @@
         odom->orientation = 0.0;
     }
 
-    void configure_diff_drive_odom(DiffDriveOdom* odom, float loop_period, float wheel_radius, float wheel_separation) {
-        odom->loop_period = loop_period;
+    void configure_diff_drive_odom(DiffDriveOdom* odom, float odom_update_period, float wheel_radius, float wheel_separation) {
+        odom->odom_update_period = odom_update_period;
         odom->wheel_radius = wheel_radius;
         odom->wheel_separation = wheel_separation;
         reset_odometry(odom);
     }
 
-    void update_odometry(DiffDriveOdom* odom, float left_wheel_pos, float right_wheel_pos) {
-        if (odom->loop_period <= 0) return;
+    void update_odometry(DiffDriveOdom* odom, float left_wheel_feedback_vel, float right_wheel_feedback_vel) {
+        if (odom->odom_update_period <= 0) return;
 
         // Calculating wheel velocity
-        odom->left_wheel_pos = left_wheel_pos * odom->wheel_radius;
-        odom->right_wheel_pos = right_wheel_pos * odom->wheel_radius;
-
-        odom->left_wheel_vel = (odom->left_wheel_pos - odom->old_left_wheel_pos) / odom->loop_period;
-        odom->right_wheel_vel = (odom->right_wheel_pos - odom->old_right_wheel_pos) / odom->loop_period;
-
-        odom->old_left_wheel_pos = odom->left_wheel_pos;
-        odom->old_right_wheel_pos = odom->right_wheel_pos;
+        odom->left_wheel_vel_mps = left_wheel_feedback_vel * odom->wheel_radius;
+        odom->right_wheel_vel_mps = right_wheel_feedback_vel * odom->wheel_radius;
 
         // Estimate robot speed and position
-        odom->linear_x = (odom->right_wheel_vel + odom->left_wheel_vel) * 0.5;
-        odom->angular_z = (odom->right_wheel_vel - odom->left_wheel_vel) / odom->wheel_separation;
+        odom->linear_x = (odom->right_wheel_vel_mps + odom->left_wheel_vel_mps) * 0.5;
+        odom->angular_z = (odom->right_wheel_vel_mps - odom->left_wheel_vel_mps) / odom->wheel_separation;
 
-        odom->pos_x += odom->linear_x * odom->loop_period * cos(odom->orientation);
-        odom->pos_y += odom->linear_x * odom->loop_period * sin(odom->orientation);
-        odom->orientation += odom->angular_z * odom->loop_period;
+        odom->pos_x += odom->linear_x * odom->odom_update_period * cos(odom->orientation);
+        odom->pos_y += odom->linear_x * odom->odom_update_period * sin(odom->orientation);
+        odom->orientation += odom->angular_z * odom->odom_update_period;
     }
 
-    float get_pos_x(DiffDriveOdom* odom) {
-        return odom->pos_x;
+    float get_odom_pos_x(DiffDriveOdom odom) {
+        return odom.pos_x;
     }
 
-    float get_pos_y(DiffDriveOdom* odom) {
-        return odom->pos_y;
+    float get_odom_pos_y(DiffDriveOdom odom) {
+        return odom.pos_y;
     }
 
-    float get_orientation(DiffDriveOdom* odom) {
-        return odom->orientation;
+    float get_odom_orientation(DiffDriveOdom odom) {
+        return odom.orientation;
     }
 
-    float get_vel_lin_x(DiffDriveOdom* odom) {
-        return odom->linear_x;
+    float get_odom_vel_lin_x(DiffDriveOdom odom) {
+        return odom.linear_x;
     }
 
-    float get_vel_ang_z(DiffDriveOdom* odom) {
-        return odom->angular_z;
+    float get_odom_vel_ang_z(DiffDriveOdom odom) {
+        return odom.angular_z;
     }
 
 #endif
